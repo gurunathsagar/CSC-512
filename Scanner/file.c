30e753ba9ca9b47134cb53ae32b41c9481d67262
Gurunath <gurunath.sagar@gmail.com>
2016-09-11 18:26:53 -0400

    added feile

---
+++ Scanner/file.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#define read(x) scanf("%d\n", &x) 
+#define write(x) printf("%d\n", x)
+// function foo 
+!=
+void foo() { 
+    int a_b = -1 ; 
+    read(a);
+    write(a); 
+}
+
+int main() { 
+    foo(); 
+}
