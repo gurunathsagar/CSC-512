e6f5b8b514d09781d0b13cd80f80e6140d497abe
Gurunath Hanamsagar <gurunath.sagar@gmail.com>
2016-11-12 16:20:12 +0000

    Added files file.c and README

---
+++ IR/Parser/file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define read(x) scanf("%d",&x)
+#define write(x) printf("%d\n",x)
+int m,n;
+int p,q,l,j,h;
+int xyz(int p, int u);
+int add(int a) {
+  int b,k,i;
+  b = (k+i/2);
+  
+  
+}
